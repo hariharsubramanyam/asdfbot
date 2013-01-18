88f86108d9e8c713c0def23ce0ff1b5f298a718a
Trevor Mulchay <tmulchay@mit.edu>
2013-01-18 00:45:36 -0500

    Created trevPlayer2

---
+++ trevPlayer2/SNukeTransition.java
@@ -0,0 +1,31 @@
+package trevPlayer2;
+
+import battlecode.common.GameActionException;
+
+public class SNukeTransition extends Transition {
+	
+	public SNukeTransition(StateMachine rootSM){
+		this.rootSM = rootSM;
+		this.sourceState = SMConstants.SWAITSTATE;
+		this.targetState = SMConstants.SATTACKSTATE;
+	}
+
+	@Override
+	public int getSourceStateID() {
+		// TODO Auto-generated method stub
+		return sourceState;
+	}
+
+	@Override
+	public State getTargetState() {
+		return SMConstants.getState(rootSM, targetState);
+	}
+
+	@Override
+	public boolean isTriggered() {
+		try {
+			return rootSM.rc.senseEnemyNukeHalfDone();
+		} catch (GameActionException e) {return false;}
+	}
+
+}
